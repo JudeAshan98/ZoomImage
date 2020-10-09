83a1b532f4c7a0aadf73f7466204d2fb79cd9fcf
JudeAshan98 <judeashanlakmal356@gmail.com>
2020-10-09 17:29:02 +0530

    Added new class for Zoom and Move

---
+++ app/src/main/java/com/example/zoomimage/TestClass.java
@@ -0,0 +1,104 @@
+package com.example.zoomimage;
+
+import android.graphics.Matrix;
+import android.graphics.PointF;
+import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TableLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TestClass extends AppCompatActivity implements View.OnTouchListener {
+
+    // We can be in one of these 3 states
+    static final int NONE = 0;
+    static final int DRAG = 1;
+    static final int ZOOM = 2;
+    Matrix matrix = new Matrix();
+    Matrix savedMatrix = new Matrix();
+    int mode = NONE;
+    // Remember some things for zooming
+    PointF start = new PointF();
+    PointF mid = new PointF();
+    float oldDist = 1f;
+    private ImageView mOrologio;
+    private FrameLayout mPhotoBox;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.framelayout);
+        MyImageView myImageView = new MyImageView(this, (AttributeSet) mOrologio,1);
+
+    }
+
+
+    private void bindViews(View rootView) {
+        mPhotoBox = (FrameLayout) rootView.findViewById(R.id.fotoBox);
+
+        mOrologio = (ImageView) rootView.findViewById(R.id.imgOrologio);
+        mOrologio.setOnTouchListener(this);
+    }
+
+    private float spacing(MotionEvent event) {
+        float x = event.getX(0) - event.getX(1);
+        float y = event.getY(0) - event.getY(1);
+        return (float) Math.sqrt(x * x + y * y);
+    }
+
+    private void midPoint(PointF point, MotionEvent event) {
+        float x = event.getX(0) + event.getX(1);
+        float y = event.getY(0) + event.getY(1);
+        point.set(x / 2, y / 2);
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        ImageView view = (ImageView) v;
+
+        switch (event.getAction() & MotionEvent.ACTION_MASK) {
+            case MotionEvent.ACTION_DOWN:
+                savedMatrix.set(matrix);
+                start.set(event.getX(), event.getY());
+                mode = DRAG;
+                break;
+            case MotionEvent.ACTION_POINTER_DOWN:
+                oldDist = spacing(event);
+                if (oldDist > 10f) {
+                    savedMatrix.set(matrix);
+                    midPoint(mid, event);
+                    mode = ZOOM;
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_POINTER_UP:
+                mode = NONE;
+                break;
+            case MotionEvent.ACTION_MOVE:
+                if (mode == DRAG) {
+                    // ...
+                    matrix.set(savedMatrix);
+                    matrix.postTranslate(event.getX() - start.x,
+                            event.getY() - start.y);
+                } else if (mode == ZOOM) {
+                    float newDist = spacing(event);
+                    if (newDist > 10f) {
+                        matrix.set(savedMatrix);
+                        float scale = newDist / oldDist;
+                        matrix.postScale(scale, scale, mid.x, mid.y);
+                    }
+                }
+                break;
+        }
+
+        view.setImageMatrix(matrix);
+        return true; // indicate event was handled
+    }
+}
+
+
